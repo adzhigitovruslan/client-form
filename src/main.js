d44351998f50a5eb655bc88c5b75ac86c937fd59
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-11-14 15:55:57 +0300

    feat: add vuelidate

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
+Vue.use(Vuelidate)
 
 const app = new Vue({
   render: h => h(App),
